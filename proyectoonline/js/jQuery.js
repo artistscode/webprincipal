538e464aba01db4d5844f79cc88afa49b95fc7a4
JelkinsP <JelkinsP@DESKTOP-SQDGT4E>
2017-07-18 18:23:21 +0200

    restauracion

--- proyectoonline/js/jQuery.js
+++ proyectoonline/js/jQuery.js
@@ -4,10 +4,4 @@ $(document).ready(function () {
     $("#btnMenu").click(function () {
         $("#menu").slideToggle("slow");
     });
-    $("#btnLeft").click(function () {
-        $("#sldSize").slidesjs({
-            width: '100%',
-            height: 200
-        });
-    });
 });
