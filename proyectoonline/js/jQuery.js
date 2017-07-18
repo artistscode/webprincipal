6b80df461a1057d61df62f26e88ffdfec676abbd
JelkinsP <JelkinsP@DESKTOP-SQDGT4E>
2017-07-18 18:16:12 +0200

    resolver jquery

--- proyectoonline/js/jQuery.js
+++ proyectoonline/js/jQuery.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
     $("#btnMenu").click(function () {
         $("#menu").slideToggle("slow");
     });
+    $("#btnLeft").click(function () {
+        $("#sldSize").slidesjs({
+            width: '100%',
+            height: 200
+        });
+    });
 });
