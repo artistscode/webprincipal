844df4278ba6710e28ee812001ba8e8995301535
Clipo0 <Clipo0@DESKTOP-HKQ02SI>
2017-07-19 14:11:01 +0200

    modificacion del jquery para que solo rote sobre las imagenes establecidas y corrección de los botones

--- proyectoonline/js/jQuery.js
+++ proyectoonline/js/jQuery.js
@@ -1,14 +1,20 @@
-
+var pos=0;
 // Despliega el menu y lo repliega
 $(document).ready(function () {
     $("#btnMenu").click(function () {
         $("#menu").slideToggle("slow");
     });
     $("#btnRight").click(function () {
-        $("#sldSize").animate({left: "+=100%"}, 500);
+        if(pos>-1){
+        $("#sldSize").animate({left: "-=100%"}, 500);
+        pos--;
+        }
     });
     $("#btnLeft").click(function () {
-        $("#sldSize").animate({left: "-=100%"}, 500);
+        if(pos<1){
+        $("#sldSize").animate({left: "+=100%"}, 500);
+        pos++;
+        }
     });
 });
 
