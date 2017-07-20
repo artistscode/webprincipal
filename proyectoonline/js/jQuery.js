6b0a7b1ec2c3a183f35a940e648793e07a62f994
Clipo0 <Clipo0@DESKTOP-HKQ02SI>
2017-07-20 19:18:37 +0200

    ocultacion del slide con el menu activo

--- proyectoonline/js/jQuery.js
+++ proyectoonline/js/jQuery.js
@@ -5,6 +5,14 @@ $(document).ready(function () {
     $("#btnMenu").click(function () {
         $("#menu").slideToggle("slow");
         $("#sldSize, #btnRight, #btnLeft").toggleClass("sldSizeNone");
+        //Comprobación si el slide tiene entre sus clases la clase "sldSizeNone"
+        if($("#sldSize, #btnRight, #btnLeft").hasClass("sldSizeNone")){
+            //Desaparece el slide cuando el menu esta activo con fadeOut
+            $("#sldSize, #btnRight, #btnLeft").fadeOut("slow");
+        }else{
+            //Aparece el slide cuando el menu esta oculto con fadeIn
+            $("#sldSize, #btnRight, #btnLeft").fadeIn("slow");
+        }
     });
     $("#btnRight").click(function () {
         //Compruebo que la posición de la foto que se muestra no sea la primera
